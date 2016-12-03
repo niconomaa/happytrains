d52d56e3ca73ee0e2f2ba7c36fe42bc9e0b955eb
Nico Scordialo <nicoscordialo@googlemail.com>
2016-12-03 21:25:40 +0100

    neu

---
+++ trainlines.py
@@ -0,0 +1,75 @@
+import json
+from graphqlclient import GraphQLClient
+
+client = GraphQLClient('https://api.graph.cool/simple/v1/ciw9brm021bfh0171mp8tiric')
+
+# for each train line:
+    # get all average values
+
+get_all_trainslines_query = '''
+query
+all {
+  allTrainlines {
+    name
+    id
+    avgAnger
+    avgSadness
+    avgJoy
+    avgFear
+    avgDisgust
+  }
+}
+'''
+all_trainlines = json.loads(client.execute(get_all_trainslines_query))["data"]["allTrainlines"]
+
+
+
+# get all answers and match them to their respective train line
+
+get_all_answers_query = '''
+query
+all {
+  allAnswers {
+    trainline{
+        name
+    }
+    text
+  }
+}
+'''
+
+all_answers = json.loads(client.execute(get_all_answers_query))["data"]["allAnswers"]
+
+
+
+overview = {}
+
+for trainline in all_trainlines:
+    # print("""
+    # The average joy of people riding on {} is {}%. \n
+    # The average anger of people riding on {} is {}%. \n\n
+    # """.format(
+    # trainline["name"], float(trainline["avgJoy"]) * 100,
+    # trainline["name"], float(trainline["avgAnger"]) * 100,
+    # ))
+    # print("""
+    # People are saying the following things about {}
+    # """.format(trainline["name"]))
+    overview[trainline["name"]] = {}
+    overview[trainline["name"]]["avgSadness"] = trainline["avgSadness"]
+    overview[trainline["name"]]["avgFear"] = trainline["avgFear"]
+    overview[trainline["name"]]["avgDisgust"] = trainline["avgDisgust"]
+    overview[trainline["name"]]["avgJoy"] = trainline["avgJoy"]
+    overview[trainline["name"]]["answers"] = []
+    for answer in all_answers:
+        if(answer["trainline"]["name"] == trainline["name"]):
+            # print(answer["text"] + "\n")
+            overview[trainline["name"]]["answers"].append(answer["text"])
+
+for key, val in overview.items():
+    print(key,val)
+
+# write all  analysis to a json file
+
+with open('data.js', 'w') as outfile:
+    json.dump(overview, outfile)
--- watson.py
+++ watson.py
@@ -5,52 +5,60 @@ from graphqlclient import GraphQLClient
 client = GraphQLClient('https://api.graph.cool/simple/v1/ciw9brm021bfh0171mp8tiric')
 
 # get all answer ids and texts that dont have any evaluationy yet
-    # store those in dict: id:text
+
+get_all_answers_query = '''
+query
+all {
+  allAnswers {
+    id
+    text
+  }
+}
+'''
+all_answers = json.loads(client.execute(get_all_answers_query))["data"]["allAnswers"]
+
+for answer in all_answers:
+    print(answer)
+
 
 #  analyze text for all ideas and write analysis to answer
 
-get_all_answers_query =
+for answer in all_answers:
+
+    tone_analyzer = ToneAnalyzerV3(
+       username='b89a4c6a-673e-46b8-aa66-9b0272ea4c52',
+       password='dFo4Er2Njuht',
+       version='2016-05-19')
+
+    text = answer["text"]
+
+    response = json.dumps(tone_analyzer.tone(text=text, tones = "emotion"))
+
+    responsedict = json.loads(response)
+
+    emotions = {}
+
+    for key, val in responsedict.items():
+        if type(val) is dict:
+            for valkey, valval in val.items():
+                for item in valval:
+                    for itemkey, itemval in item.items():
+                        if type(itemval) is list:
+                            for item in itemval:
+                                emotions[item['tone_id']] = item["score"]
+
+
+    query = '''
+    mutation {{
+      updateAnswer(id: "{}" sadness: {} disgust: {} anger: {} joy: {}, fear: {}) {{
+        text
+        anger
+        joy
+      }}
+    }}
+    '''.format(answer["id"], emotions['sadness'], emotions['disgust'], emotions['anger'], emotions['joy'], emotions["fear"])
 
 
 
-
-tone_analyzer = ToneAnalyzerV3(
-   username='b89a4c6a-673e-46b8-aa66-9b0272ea4c52',
-   password='dFo4Er2Njuht',
-   version='2016-05-19')
-
-text = response = raw_input("How was your train ride today?")
-
-
-response = json.dumps(tone_analyzer.tone(text=text, tones = "emotion"))
-
-responsedict = json.loads(response)
-
-emotions = {}
-
-for key, val in responsedict.items():
-    if type(val) is dict:
-        for valkey, valval in val.items():
-            for item in valval:
-                for itemkey, itemval in item.items():
-                    if type(itemval) is list:
-                        for item in itemval:
-                            emotions[item['tone_id']] = item["score"]
-
-for key in emotions:
-    print(key)
-
-
-query = '''
-mutation {{
-  updateAnswer(id: "ciw9c5qi1008l0164mhnm7vhs" sadness: {} disgust: {} anger: {} joy: {}) {{
-    sadness
-    disgust
-  }}
-}}
-'''.format(emotions['sadness'], emotions['disgust'], emotions['anger'], emotions['joy'])
-
-
-
-result = client.execute(query)
-print(result)
+    result = client.execute(query)
+    print(result)
